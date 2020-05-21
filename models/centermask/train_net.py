ba6522e8002ece8c3d238c4e601ed77c7fe8b8ac
Dmitry <shendryk.d@pg.com>
2020-05-21 18:42:57 +0300

    add main

--- models/centermask/train_net.py
+++ models/centermask/train_net.py
@@ -21,16 +21,16 @@ from detectron2.evaluation import (
 )
 import sys
 
-ROOT = os.path.abspath('../../')
+
 
 # sys.path.append(ROOT)
 
-from ..centermask.centermask.evaluation import COCOEvaluator
+from centermask.evaluation import COCOEvaluator
 from detectron2.modeling import GeneralizedRCNNWithTTA
 
 from detectron2.data.dataset_mapper import DatasetMapper
-from ..centermask.centermask.config import get_cfg
-from ..centermask.centermask.checkpoint import AdetCheckpointer
+from centermask.config import get_cfg
+from centermask.checkpoint import AdetCheckpointer
 
 from detectron2.data import DatasetCatalog, MetadataCatalog
 
