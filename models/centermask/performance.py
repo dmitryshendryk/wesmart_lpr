313fdb2f6dd27ef818c789946316c4546d0dce5e
Dmitry <shendryk.d@pg.com>
2020-05-23 06:59:31 +0300

    update data handler

--- models/centermask/performance.py
+++ models/centermask/performance.py
@@ -19,53 +19,55 @@ CONFIG = 'config'
 WEIGHTS = 'weights'
 DEVICE = 'cuda'
 
-def get_carplate_dicts(mode):
-    path = os.path.join(ROOT, DATA_FOLDER)
-    json_file = os.path.join(path, "dataset_mask_full_no_aug.json")
-    with open(json_file) as f:
-        imgs_anns = json.load(f)
+from data_handler.dataset_handler import get_carplate_dicts
+
+# def get_carplate_dicts(mode):
+#     path = os.path.join(ROOT, DATA_FOLDER)
+#     json_file = os.path.join(path, "dataset_mask_full_no_aug.json")
+#     with open(json_file) as f:
+#         imgs_anns = json.load(f)
     
-    dataset_dicts = []
-    dataset_len = len(list(imgs_anns['_via_img_metadata'].values()))
-    dataset = list(imgs_anns['_via_img_metadata'].values())
-    if mode == 'train':
-        dataset = dataset[:dataset_len - int(dataset_len*0.1)]
-    elif mode == 'val':
-        dataset = dataset[dataset_len - int(dataset_len*0.1):]
+#     dataset_dicts = []
+#     dataset_len = len(list(imgs_anns['_via_img_metadata'].values()))
+#     dataset = list(imgs_anns['_via_img_metadata'].values())
+#     if mode == 'train':
+#         dataset = dataset[:dataset_len - int(dataset_len*0.1)]
+#     elif mode == 'val':
+#         dataset = dataset[dataset_len - int(dataset_len*0.1):]
 
-    print(len(dataset))
-    for idx, v in enumerate(list(dataset)):
-        record = {}
+#     print(len(dataset))
+#     for idx, v in enumerate(list(dataset)):
+#         record = {}
         
-        filename = os.path.join(path, v["filename"])
-        height, width = cv2.imread(filename).shape[:2]
+#         filename = os.path.join(path, v["filename"])
+#         height, width = cv2.imread(filename).shape[:2]
         
-        record["file_name"] = filename
-        record["image_id"] = idx
-        record["height"] = height
-        record["width"] = width
+#         record["file_name"] = filename
+#         record["image_id"] = idx
+#         record["height"] = height
+#         record["width"] = width
       
-        annos = v["regions"]
-        objs = []
-        for anno in annos:
-            assert not anno["region_attributes"]
-            anno = anno["shape_attributes"]
-            px = anno["all_points_x"]
-            py = anno["all_points_y"]
-            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
-            poly = [p for x in poly for p in x]
+#         annos = v["regions"]
+#         objs = []
+#         for anno in annos:
+#             assert not anno["region_attributes"]
+#             anno = anno["shape_attributes"]
+#             px = anno["all_points_x"]
+#             py = anno["all_points_y"]
+#             poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
+#             poly = [p for x in poly for p in x]
 
-            obj = {
-                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
-                "bbox_mode": BoxMode.XYXY_ABS,
-                "segmentation": [poly],
-                "category_id": 0,
-                "iscrowd": 0
-            }
-            objs.append(obj)
-        record["annotations"] = objs
-        dataset_dicts.append(record)
-    return dataset_dicts
+#             obj = {
+#                 "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
+#                 "bbox_mode": BoxMode.XYXY_ABS,
+#                 "segmentation": [poly],
+#                 "category_id": 0,
+#                 "iscrowd": 0
+#             }
+#             objs.append(obj)
+#         record["annotations"] = objs
+#         dataset_dicts.append(record)
+#     return dataset_dicts
 
 
 DatasetCatalog.register("carplate_train", lambda x='train':  get_carplate_dicts(x))
