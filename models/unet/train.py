830a54ce67130d362425eca9fe52a89a02da9f4f
Dmitry <shendryk.d@pg.com>
2020-05-25 17:06:43 +0300

    update

--- models/unet/train.py
+++ models/unet/train.py
@@ -130,13 +130,13 @@ def get_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
                         help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
                         help='Batch size', dest='batchsize')
     parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.01,
                         help='Learning rate', dest='lr')
     parser.add_argument('-f', '--load', dest='load', type=str, default=False,
                         help='Load model from a .pth file')
-    parser.add_argument('-s', '--scale', dest='scale', type=float, default=0.3,
+    parser.add_argument('-s', '--scale', dest='scale', type=float, default=0.6,
                         help='Downscaling factor of the images')
     parser.add_argument('-v', '--validation', dest='val', type=float, default=0.1,
                         help='Percent of the data that is used as validation (0-100)')
