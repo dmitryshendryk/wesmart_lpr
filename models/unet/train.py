465b05ad83e090aa79ad3de6b9c87574c22764e1
Dmitry <shendryk.d@pg.com>
2020-05-25 07:23:00 +0300

    fix val percent

--- models/unet/train.py
+++ models/unet/train.py
@@ -24,7 +24,7 @@ dir_checkpoint = 'checkpoints/'
 def train_net(net,
               device,
               epochs=5,
-              batch_size=1,
+              batch_size=0.1,
               lr=0.001,
               val_percent=0.01,
               save_cp=True,
@@ -34,8 +34,8 @@ def train_net(net,
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train, val = random_split(dataset, [n_train, n_val])
-    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=2)
-    val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
+    val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True, drop_last=True)
 
     writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
     global_step = 0
