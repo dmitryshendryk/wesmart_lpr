d5c19af73f2aac080500497952b80aac21b92f45
Dmitry <shendryk.d@pg.com>
2020-05-25 08:11:54 +0300

    fix val percent

--- models/unet/train.py
+++ models/unet/train.py
@@ -66,6 +66,8 @@ def train_net(net,
             for batch in train_loader:
                 imgs = batch['image']
                 true_masks = batch['mask']
+                if imgs.shape[1] == 4:
+                    print(imgs.shape)
                 assert imgs.shape[1] == net.n_channels, \
                     f'Network has been defined with {net.n_channels} input channels, ' \
                     f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
