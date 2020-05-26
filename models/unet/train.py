2552dcf8866ad8ddc364145721f44a25658152fb
Dmitry <shendryk.d@pg.com>
2020-05-26 15:18:55 +0300

    add datasets

--- models/unet/train.py
+++ models/unet/train.py
@@ -267,7 +267,7 @@ def train_model(model, optimizer, scheduler, num_epochs=25):
 
                 # statistics
                 epoch_samples += inputs.size(0)
-                print(epoch_samples)
+                print(epoch_samples, metrics['loss'])
 
             print_metrics(metrics, epoch_samples, phase)
             epoch_loss = metrics['loss'] / epoch_samples
