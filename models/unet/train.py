0dd3453d80c40485f668f936e988c2d4a53ba10c
Dmitry <shendryk.d@pg.com>
2020-05-26 15:32:41 +0300

    add datasets

--- models/unet/train.py
+++ models/unet/train.py
@@ -60,17 +60,17 @@ trans = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # imagenet
 ])
 
-train_set = SimDataset(2000, transform=trans)
-val_set = SimDataset(200, transform=trans)
+# train_set = SimDataset(2000, transform=trans)
+# val_set = SimDataset(200, transform=trans)
 
 dir_img = 'data/imgs/'
 dir_mask = 'data/masks/'
 
-# dataset = BasicDataset(dir_img, dir_mask, trans, scale=0.5)
+dataset = BasicDataset(dir_img, dir_mask, trans, scale=0.5)
 val_percent=0.1
-# n_val = int(len(dataset) * val_percent)
-# n_train = len(dataset) - n_val
-# train_set, val_set = random_split(dataset, [n_train, n_val])
+n_val = int(len(dataset) * val_percent)
+n_train = len(dataset) - n_val
+train_set, val_set = random_split(dataset, [n_train, n_val])
 batch_size = 25
 
 
@@ -286,7 +286,7 @@ def train_model(model, optimizer, scheduler, num_epochs=25):
 
 print(device)
 
-num_class = 6
+num_class = 1
 
 model = ResNetUNet(num_class).to(device)
 
