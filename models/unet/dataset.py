20dd46688f811fee7a27e9c155de3aa0cb54948f
Dmitry <shendryk.d@pg.com>
2020-05-26 16:57:02 +0300

    add datasets

--- models/unet/dataset.py
+++ models/unet/dataset.py
@@ -55,7 +55,7 @@ class BasicDataset(Dataset):
 
         assert img.size == mask.size, \
             f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
-        img = self.preprocess(img, self.scale)
+        # img = self.preprocess(img, self.scale)
         mask = self.preprocess(mask, self.scale)
         print(img.shape, mask.shape)
         if img.shape[0] !=3:
