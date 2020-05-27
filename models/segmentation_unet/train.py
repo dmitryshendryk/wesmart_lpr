086fb0079d26c7c07838b9a574eecc2ab68d64b4
Dmitry <shendryk.d@pg.com>
2020-05-27 14:41:05 +0300

    segemnted unet

--- models/segmentation_unet/train.py
+++ models/segmentation_unet/train.py
@@ -95,7 +95,7 @@ def get_training_augmentation():
 
 #         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
 
-        albu.PadIfNeeded(min_height=1320, min_width=1320, always_apply=True),
+        albu.PadIfNeeded(min_height=320, min_width=320, always_apply=True),
 #         albu.RandomCrop(height=1000, width=1000, always_apply=True),
 
         albu.IAAAdditiveGaussianNoise(p=0.2),
@@ -133,7 +133,7 @@ def get_training_augmentation():
 def get_validation_augmentation():
     """Add paddings to make image shape divisible by 32"""
     test_transform = [
-        albu.PadIfNeeded(1320, 1320)
+        albu.PadIfNeeded(384, 480)
     ]
     return albu.Compose(test_transform)
 
