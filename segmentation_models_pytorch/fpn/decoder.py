fea376d9b3443210506c3f7ceae74706c66f58b3
Dmitry <shendryk.d@pg.com>
2020-06-05 18:17:35 +0300

    change lr

--- segmentation_models_pytorch/fpn/decoder.py
+++ segmentation_models_pytorch/fpn/decoder.py
@@ -21,8 +21,8 @@ class Conv3x3GNReLU(nn.Module):
     def forward(self, x):
         x = self.block(x)
         if self.upsample:
-            x = self.my_upsample_emulator(x)
-            # x = F.interpolate(x, scale_factor=2, mode="bilinear", align_corners=True)
+            # x = self.my_upsample_emulator(x)
+            x = F.interpolate(x, scale_factor=2, mode="bilinear", align_corners=True)
         return x
 
 
@@ -33,8 +33,8 @@ class FPNBlock(nn.Module):
         self.my_upsample_emulator = nn.ConvTranspose2d(pyramid_channels, pyramid_channels, kernel_size=2, stride=2)
 
     def forward(self, x, skip=None):
-        x = self.my_upsample_emulator(x)
-        # x = F.interpolate(x, scale_factor=2, mode="nearest")
+        # x = self.my_upsample_emulator(x)
+        x = F.interpolate(x, scale_factor=2, mode="nearest")
         skip = self.skip_conv(skip)
         x = x + skip
         return x
