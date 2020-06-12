d32fead7b51d0ef628863a3ca1d35f4058e71a4a
Dmitry <shendryk.d@pg.com>
2020-06-12 17:45:00 +0300

    remove imports

--- tensorrt/torch2onnx.py
+++ tensorrt/torch2onnx.py
@@ -27,7 +27,7 @@ torch.onnx.export(torch_model,               # model being run
                   x,                         # model input (or a tuple for multiple inputs)
                   "../models/segmentation_unet/super_resolution.onnx",   # where to save the model (can be a file or file-like object)
                   export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=10,          # the ONNX version to export the model to
+                  opset_version=11,          # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
                   input_names = ['input'],   # the model's input names
                   output_names = ['output'], # the model's output names
