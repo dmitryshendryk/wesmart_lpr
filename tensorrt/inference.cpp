f4ab60314ed6c4a4f3c1c9422c02ff4f75892ab7
Dmitry <shendryk.d@pg.com>
2020-06-16 11:48:44 +0300

    debug infer

--- tensorrt/inference.cpp
+++ tensorrt/inference.cpp
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
 	unique_ptr<float[]> boxes(new float[num_det * 4]);
 	unique_ptr<float[]> classes(new float[num_det]);
 
-	vector<float[]> *data_results = new vector<float[]>[dataSize];
+	vector<float[]> data_results = new vector<float[]>[dataSize];
 
 	// cudaMemcpy(scores.get(), scores_d, sizeof(float) * num_det, cudaMemcpyDeviceToHost);
 	// cudaMemcpy(boxes.get(), boxes_d, sizeof(float) * num_det * 4, cudaMemcpyDeviceToHost);
