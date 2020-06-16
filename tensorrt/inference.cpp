793794f7b21c96bb083cf2a7c9ee52c9f942c041
Dmitry <shendryk.d@pg.com>
2020-06-16 10:48:08 +0300

    debug infer

--- tensorrt/inference.cpp
+++ tensorrt/inference.cpp
@@ -16,6 +16,8 @@
 using namespace std;
 using namespace cv;
 
+#define N 1000
+
 int main(int argc, char *argv[]) {
 	if (argc<3 || argc>4) {
 		cerr << "Usage: " << argv[0] << " engine.plan image.jpg [<OUTPUT>.png]" << endl;
@@ -52,15 +54,13 @@ int main(int argc, char *argv[]) {
 		}
 	}        
 
-	// Create device buffers
 	void *data_d, *scores_d, *boxes_d, *classes_d;
 	auto num_det = engine.getMaxDetections();
 	cudaMalloc(&data_d, 3 * inputSize[0] * inputSize[1] * sizeof(float));
-	cudaMalloc(&scores_d, num_det * sizeof(float));
-	cudaMalloc(&boxes_d, num_det * 4 * sizeof(float));
-	cudaMalloc(&classes_d, num_det * sizeof(float));
+	// cudaMalloc(&scores_d, num_det * sizeof(float));
+	// cudaMalloc(&boxes_d, num_det * 4 * sizeof(float));
+	// cudaMalloc(&classes_d, num_det * sizeof(float));
 
-	// Copy image to device
 	size_t dataSize = data.size() * sizeof(float);
 	cudaMemcpy(data_d, data.data(), dataSize, cudaMemcpyHostToDevice);
 
@@ -68,7 +68,8 @@ int main(int argc, char *argv[]) {
 	cout << "Running inference..." << endl;
 	const int count = 100;
 	auto start = chrono::steady_clock::now();
- 	vector<void *> buffers = { data_d, scores_d, boxes_d, classes_d };
+ 	// vector<void *> buffers = { data_d, scores_d, boxes_d, classes_d };
+	 vector<void *> buffers = { data_d};
 	for (int i = 0; i < count; i++) {
 		engine.infer(buffers);
 	}
@@ -76,39 +77,41 @@ int main(int argc, char *argv[]) {
 	auto timing = chrono::duration_cast<chrono::duration<double>>(stop - start);
 	cout << "Took " << timing.count() / count << " seconds per inference." << endl;
 
-	cudaFree(data_d);
+	// cudaFree(data_d);
 
-	// Get back the bounding boxes
 	unique_ptr<float[]> scores(new float[num_det]);
 	unique_ptr<float[]> boxes(new float[num_det * 4]);
 	unique_ptr<float[]> classes(new float[num_det]);
-	cudaMemcpy(scores.get(), scores_d, sizeof(float) * num_det, cudaMemcpyDeviceToHost);
-	cudaMemcpy(boxes.get(), boxes_d, sizeof(float) * num_det * 4, cudaMemcpyDeviceToHost);
-	cudaMemcpy(classes.get(), classes_d, sizeof(float) * num_det, cudaMemcpyDeviceToHost);
+	cudaMemcpy(data.get(), data_d, dataSize, cudaMemcpyDeviceToHost);
+	// cudaMemcpy(boxes.get(), boxes_d, sizeof(float) * num_det * 4, cudaMemcpyDeviceToHost);
+	// cudaMemcpy(classes.get(), classes_d, sizeof(float) * num_det, cudaMemcpyDeviceToHost);
 
-	cudaFree(scores_d);
-	cudaFree(boxes_d);
-	cudaFree(classes_d);
+	cudaFree(data_d);
 
-	for (int i = 0; i < num_det; i++) {
-		// Show results over confidence threshold
-		if (scores[i] >= 0.3f) {
-			float x1 = boxes[i*4+0];
-			float y1 = boxes[i*4+1];
-			float x2 = boxes[i*4+2];
-			float y2 = boxes[i*4+3];
-			cout << "Found box {" << x1 << ", " << y1 << ", " << x2 << ", " << y2
-				<< "} with score " << scores[i] << " and class " << classes[i] << endl;
+	for (std::vector<float>::const_iterator i = data.begin(); i != data.end(); ++i)
+    	std::cout << *i << ' ';
+	// cudaFree(boxes_d);
+	// cudaFree(classes_d);
 
-			// Draw bounding box on image
-			cv::rectangle(image, Point(x1, y1), Point(x2, y2), cv::Scalar(0, 255, 0));
-		}
-	}
+	// for (int i = 0; i < num_det; i++) {
+	// 	// Show results over confidence threshold
+	// 	if (scores[i] >= 0.3f) {
+	// 		float x1 = boxes[i*4+0];
+	// 		float y1 = boxes[i*4+1];
+	// 		float x2 = boxes[i*4+2];
+	// 		float y2 = boxes[i*4+3];
+	// 		cout << "Found box {" << x1 << ", " << y1 << ", " << x2 << ", " << y2
+	// 			<< "} with score " << scores[i] << " and class " << classes[i] << endl;
+
+	// 		// Draw bounding box on image
+	// 		cv::rectangle(image, Point(x1, y1), Point(x2, y2), cv::Scalar(0, 255, 0));
+	// 	}
+	// }
 
 	// Write image
-	string out_file = argc == 4 ? string(argv[3]) : "detections.png";
-	cout << "Saving result to " << out_file << endl;
-	imwrite(out_file, image);
+	// string out_file = argc == 4 ? string(argv[3]) : "detections.png";
+	// cout << "Saving result to " << out_file << endl;
+	// imwrite(out_file, image);
 	
 	return 0;
 }
