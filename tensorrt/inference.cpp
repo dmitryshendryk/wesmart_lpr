fc521adb08130573fdf32524b1a33182bf4706fe
Dmitry <shendryk.d@pg.com>
2020-06-21 08:34:16 +0300

    logs

--- tensorrt/inference.cpp
+++ tensorrt/inference.cpp
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
 	cout << "inputSize[1]" << inputSize[1] << endl;
 	cv::Mat bgr;
 	cvtColor(m, bgr, CV_GRAY2BGR);
-	// cout << "M = " << endl << " "  << m << endl << endl;
+	cout << "M = " << endl << " "  << m << endl << endl;
 
 	// Write image
 	string out_file = argc == 4 ? string(argv[3]) : "detections.png";
